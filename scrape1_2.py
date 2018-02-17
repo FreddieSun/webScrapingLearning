aa8afebbcb31f310c1facc835565b41666a95c5b
FreddieSun <freddiesun95@gmail.com>
2018-02-17 17:55:19 -0500

    Basic use of urllib

--- scrape1_2.py
+++ scrape1_2.py
@@ -5,8 +5,27 @@ import urllib2
 # print response.read()
 
 # A better version
-request = urllib2.Request("http://www.google.com")
+# request = urllib2.Request("http://www.google.com")
+# response=urllib2.urlopen(request)
+# print response.read()
+
+
+#  Request: Post version
+
+# values={"username:":"blahblah","password":"blahblah"}
+# # coding the values(dictionary)
+# data=urllib.urlencode(values)
+# url="blahblah"
+# request=urllib2.Request(url,data)
+# response=urllib2.urlopen(request)
+# print response.read()
+
+# Request:Get version
+
+values={"username:":"freddiesun95@gmail.com","password":"blahblahblah"}
+data=urllib.urlencode(values)
+url="https://www.google.com/"
+geturl=url+"?"+data
+request=urllib2.Request(geturl)
 response=urllib2.urlopen(request)
 print response.read()
-
-
